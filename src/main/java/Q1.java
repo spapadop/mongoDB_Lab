5dea4d71d14b00fbb2c0ad7475e5cec80e9e152e
Sokratis Papadopoulos <sokpao@gmail.com>
2019-04-05 17:40:07 +0200

    starting question1

--- src/main/java/Q1.java
+++ src/main/java/Q1.java
@@ -1,28 +1,62 @@
-import util.Utils;
-import com.mongodb.ConnectionString;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
-import com.mongodb.ServerAddress;
-
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-
+import com.devskiller.jfairy.Fairy;
+import com.devskiller.jfairy.producer.company.Company;
+import com.devskiller.jfairy.producer.person.Person;
 import org.bson.Document;
-import java.util.Arrays;
-import com.mongodb.Block;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 import com.mongodb.client.MongoCursor;
-import static com.mongodb.client.model.Filters.*;
-import com.mongodb.client.result.DeleteResult;
-import static com.mongodb.client.model.Updates.*;
-import com.mongodb.client.result.UpdateResult;
-import java.util.ArrayList;
+
 import java.util.List;
+import java.util.Set;
+
+import static com.mongodb.client.model.Filters.*;
 
 public class Q1 {
 
+	private static MongoClient mongoClient;
+	private static MongoDatabase db;
+	private static MongoCollection<Document> peopleColl;
+	private static MongoCollection<Document> companyColl;
+	private static MongoCursor<Document> cursor;
 
 	public static void execute() {
+
+		mongoClient = new MongoClient();
+		db = mongoClient.getDatabase("test");
+
+		peopleColl = db.getCollection("People");
+		companyColl = db.getCollection("Companies");
+
+		cursor = peopleColl.find().iterator();
+		Document person = null;
+		Document comp = null;
+		int counter = 0;
+		String domain = null;
+		try {
+
+			while (cursor.hasNext()) {
+				person = cursor.next();
+				// bulk load or single load?? --> change to bulk
+				comp = companyColl.find(eq("name", person.get("company"))).first();
+				domain = "not found";
+				if(comp !=null) {
+					domain = comp.get("name").toString();
+				}
+				System.out.println("firstname" + person.get("firstname")
+						+ " lastname: "+ person.get("lastname") + " domain: "
+				);
+
+
+				counter++;
+			}
+		} finally {
+			cursor.close();
+		}
+		System.out.println("Read " + counter + "lines");
+
 
 	}
 
