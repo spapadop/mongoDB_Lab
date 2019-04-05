df6045efb504fd8e95b3464559f2911c4af42774
Sokratis Papadopoulos <sokpao@gmail.com>
2019-04-05 17:33:34 +0200

    populate working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,21 +4,20 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 
-		populateDB.populate(1);
 
-//		if (args.length < 1) {
-//			throw new Exception("Wrong number of parameters, usage: load N (number of documents to create) or Q1/Q2");
-//		}
-//
-//		if (args[0].equals("populate")) {
-//			if (Utils.isNumber(args[1])) populateDB.populate(Integer.parseInt(args[1]));
-//		}
-//		else if (args[0].equals("Q1")) {
-//			Q1.execute();
-//		}
-//		else if (args[0].equals("Q2")) {
-//			Q2.execute();
-//		}
+		if (args.length < 1) {
+			throw new Exception("Wrong number of parameters, usage: load N (number of documents to create) or Q1/Q2");
+		}
+
+		if (args[0].equals("populate")) {
+			if (Utils.isNumber(args[1])) populateDB.populate(Integer.parseInt(args[1]));
+		}
+		else if (args[0].equals("Q1")) {
+			Q1.execute();
+		}
+		else if (args[0].equals("Q2")) {
+			Q2.execute();
+		}
 	}
 	
 }
--- src/main/java/populateDB.java
+++ src/main/java/populateDB.java
@@ -1,32 +1,95 @@
-import util.Utils;
-import com.mongodb.ConnectionString;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
-import com.mongodb.ServerAddress;
-
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.bson.Document;
-import java.util.Arrays;
-import com.mongodb.Block;
 
-import com.mongodb.client.MongoCursor;
-import static com.mongodb.client.model.Filters.*;
-import com.mongodb.client.result.DeleteResult;
-import static com.mongodb.client.model.Updates.*;
-import com.mongodb.client.result.UpdateResult;
-import java.util.ArrayList;
-import java.util.List;
+import com.devskiller.jfairy.Fairy;
+import com.devskiller.jfairy.producer.company.Company;
+import com.devskiller.jfairy.producer.person.Person;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class populateDB {
 
+	private static MongoClient mongoClient;
+	private static MongoDatabase db;
+	private static MongoCollection<Document> peopleColl;
+	private static MongoCollection<Document> companyColl;
+
+	private static Fairy fairy;
+	private static Person person;
+	private static Company company;
+
+	private static List<Document> personDocs;
+	private static List<Document> compDocs;
+	private static Set<String> comps;
+	private static Set<String> people;
+
+
+	private static void init() {
+		mongoClient = new MongoClient();
+		db = mongoClient.getDatabase("test");
+
+		//Question. Do we need to to check the object or the name
+		//Question can we  do all of that locally and always drop
+		//the collection FIRST
+
+		peopleColl = db.getCollection("People");
+		companyColl = db.getCollection("Companies");
+		peopleColl.drop();
+		companyColl.drop();
+		peopleColl = db.getCollection("People");
+		companyColl = db.getCollection("Companies");
+
+		personDocs = new ArrayList<Document>();
+		compDocs = new ArrayList<Document>();
+		comps = new HashSet<String>();
+		people = new HashSet<String>();
+
+		fairy = Fairy.create();
+	}
+
+
 	public static void populate(int N) {
+		init();
 
-		MongoClient mongoClient = MongoClients.create();
-		MongoDatabase db = mongoClient.getDatabase("test");
+		for(int i = 0;i<N ; i++) {
+			person = fairy.person();
+			company = person.getCompany();
 
-		System.out.println(db.getCollection("People").countDocuments());
+			if(!people.contains(person.getPassportNumber())) {
+				people.add(person.getPassportNumber());
+				Document doc = new Document("passportNumber", person.getPassportNumber())
+						.append("firstname", person.getFirstName())
+						.append("lastname", person.getLastName())
+						.append("email", person.getEmail())
+						.append("age", person.getAge())
+						.append("company", company.getName());
+
+				personDocs.add(doc);
+			}
+
+			if(!comps.contains(company.getName())) {
+				comps.add(company.getName());
+				Document doc2 = new Document("name", company.getName())
+						.append("domain", company.getDomain())
+						.append("email", company.getEmail())
+						.append("url", company.getUrl());
+				compDocs.add(doc2);
+			}
+
+
+		}
+		System.out.println("Try to insert "+ personDocs.size() + " People and  "+compDocs.size() + " Companies");
+
+		peopleColl.insertMany(personDocs);
+		companyColl.insertMany(compDocs);
+
+		System.out.println("Finished Insertmethod");
 
 	}
 
